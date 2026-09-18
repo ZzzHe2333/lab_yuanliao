7c78e5791905ade447e6a52e72f57a123d8ed36d
ZzzHe <x121144@163.com>
2026-09-18 10:15:13 +0800

    ui: add frontend/src/router.js

---
+++ frontend/src/router.js
@@ -0,0 +1,20 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import DashboardView from './views/DashboardView.vue'
+import ChemicalsView from './views/ChemicalsView.vue'
+import ChemicalFormView from './views/ChemicalFormView.vue'
+import ChemicalDetailView from './views/ChemicalDetailView.vue'
+import WarehousesView from './views/WarehousesView.vue'
+import ScanView from './views/ScanView.vue'
+
+export default createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: '/', component: DashboardView },
+    { path: '/chemicals', component: ChemicalsView },
+    { path: '/chemicals/new', component: ChemicalFormView },
+    { path: '/chemicals/:id', component: ChemicalDetailView },
+    { path: '/chemicals/:id/edit', component: ChemicalFormView },
+    { path: '/warehouses', component: WarehousesView },
+    { path: '/scan', component: ScanView }
+  ]
+})
